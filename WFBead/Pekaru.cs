6b73fb18aee9992e1317f08496e1293489d6b271
KeanuReeeves <71759633+KeanuReeeves@users.noreply.github.com>
2020-11-15 18:12:55 +0100

    Add project files.

---
+++ WFBead/Pekaru.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WFBead
+{
+    class Pekaru
+    {
+        private string nev;
+        private int ar;
+        private bool laktozmentes;
+        public Pekaru()
+        { 
+        
+        }
+    }
+}
