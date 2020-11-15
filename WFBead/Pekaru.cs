715ea103ac4f5331448ee4c67283e837bbdfa8b5
KeanuReeeves <71759633+KeanuReeeves@users.noreply.github.com>
2020-11-15 18:19:25 +0100

    pekaru class

--- WFBead/Pekaru.cs
+++ WFBead/Pekaru.cs
@@ -11,9 +11,20 @@ namespace WFBead
         private string nev;
         private int ar;
         private bool laktozmentes;
-        public Pekaru(string nev)
-        { 
-        
+        public Pekaru(string nev,int ar,bool laktozmentes)
+        {
+            this.Nev = nev;
+            this.Ar = ar;
+            this.Laktozmentes = laktozmentes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Ar { get => ar; set => ar = value; }
+        public bool Laktozmentes { get => laktozmentes; set => laktozmentes = value; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1}Ft/db)",this.nev,this.ar);
         }
     }
 }
